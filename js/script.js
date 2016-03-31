4f0f03344a5b534a03326584af536436cea804b7
Oleg Kvashnin <oleg-161@yandex.ru>
2016-04-01 01:48:33 +0300

    Did not make the script yet...

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
+
 function init() {
+	/*
+	$(document).click(function(){
+		$('.abs_list_block').hide();
+	});
+	*/
+	/*
+	$('.ab_select_wrap *').click(function(e){
+		e.stopPropogation();
+	});
+	*/
+	
+	$('.ab_select_wrap').click(function(e){
+		
+		
+		var pnd = this.parentNode;
+		var alb = [];
+		alb = $(pnd).find('.abs_list_block');
+		$(alb[0]).toggle();
+		e.stopPropogation();
+	});
+	
 	
 }
